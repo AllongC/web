9546c4f26640b49748f7422faaccb695a03a0e56
杨腾祺 <3150475761@qq.com>
2020-07-18 20:45:00 +0800

    12-登录页静态渲染

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>login</div>
+  <section>
+    <div class="container">
+      <div class="content">
+        <el-row class="tab">
+          <el-col :span="12" class="active tabs">登录</el-col>
+          <el-col :span="12" class="tabs">注册</el-col>
+        </el-row>
+        <el-row class="login"></el-row>
+        <el-row class="register"></el-row>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -7,4 +18,51 @@ export default {};
 </script>
 
 <style lang="less" scoped>
+section {
+  height: 700px;
+  background: url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
+  .container {
+    position: relative;
+    height: 700px;
+    width: 1000px;
+    max-width: 1000px;
+    margin: 0 auto;
+    .content {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-left: -200px;
+      margin-top: -200px;
+    }
+    .tab {
+      width: 400px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      background-color: #eeeeee;
+      color: #333;
+      .tabs {
+        cursor: pointer;
+      }
+      .active {
+        width: 200px;
+        border-top: 2px solid #ffa500;
+        color: #ffa500;
+        font-weight: bold;
+        background-color: #fff;
+      }
+    }
+    .login {
+      width: 400px;
+      height: 240px;
+      background-color: #fff;
+    }
+    .register {
+      display: none;
+      width: 400px;
+      height: 440px;
+      background-color: #fff;
+    }
+  }
+}
 </style>

7f44a60740e345a1086e20211d8b6713f246df6e
杨腾祺 <3150475761@qq.com>
2020-07-21 20:34:48 +0800

    26-机票首页静态页面完成

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -1,5 +1,102 @@
 <template>
-  <div>air</div>
+  <section class="container">
+    <div class="header">
+      <span class="iconfont iconfeiji"></span>
+      <span>国内机票</span>
+    </div>
+    <div class="air">
+      <el-row>
+        <el-col class="form">
+          <el-row class="form-top">
+            <el-col class="current">
+              <i class="iconfont icondancheng"></i>
+              单程
+            </el-col>
+            <el-col>
+              <i class="iconfont iconshuangxiang"></i>
+              往返
+            </el-col>
+          </el-row>
+          <el-row class="form-content">
+            <el-form>
+              <el-form-item label="出发城市">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="到达城市">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="出发时间">
+                <el-date-picker type="date" placeholder="选择日期"></el-date-picker>
+              </el-form-item>
+              <el-button type="primary">
+                <i class="el-icon-search"></i>搜索
+              </el-button>
+              <div class="change">
+                <span>换</span>
+              </div>
+            </el-form>
+          </el-row>
+        </el-col>
+        <el-col class="board">
+          <img src="http://157.122.54.189:9093/images/pic_sale.jpeg" alt />
+        </el-col>
+      </el-row>
+    </div>
+    <div class="service">
+      <el-row>
+        <el-col>
+          <i class="iconfont iconweibiaoti-_huabanfuben"></i>
+          <span>100%航协认证</span>
+        </el-col>
+        <el-col>
+          <i class="iconfont iconbaozheng"></i>
+          <span>出行保证</span>
+        </el-col>
+        <el-col>
+          <i class="iconfont icondianhua"></i>
+          <span>7x24小时服务</span>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="special">
+      <div class="special-top">
+        <i class="iconfont icontejiajipiao"></i>
+        <h4>特价机票</h4>
+      </div>
+      <div class="special-bottom">
+        <el-row>
+          <el-col>
+            <img src="http://157.122.54.189:9095/assets/images/tj01.jpg" alt />
+            <p>
+              <span>广州-上海</span>
+              <span class="right">￥790</span>
+            </p>
+          </el-col>
+          <el-col>
+            <img src="http://157.122.54.189:9095/assets/images/tj01.jpg" alt />
+            <p>
+              <span>广州-上海</span>
+              <span class="right">￥790</span>
+            </p>
+          </el-col>
+          <el-col>
+            <img src="http://157.122.54.189:9095/assets/images/tj01.jpg" alt />
+            <p>
+              <span>广州-上海</span>
+              <span class="right">￥790</span>
+            </p>
+          </el-col>
+          <el-col>
+            <img src="http://157.122.54.189:9095/assets/images/tj01.jpg" alt />
+            <p>
+              <span class="left">广州-上海</span>
+              <span class="right">￥790</span>
+            </p>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -7,4 +104,184 @@ export default {};
 </script>
 
 <style lang="less" scoped>
+.container {
+  width: 1000px;
+  min-width: 1000px;
+  margin: 0 auto;
+  .header {
+    margin: 20px 0px;
+    > span {
+      font-size: 20px;
+      color: #ffa500;
+    }
+  }
+  .air {
+    height: 350px;
+    margin-bottom: 20px;
+    .form {
+      width: 360px;
+      height: 350px;
+      border: 1px solid #dddddd;
+      border-top: none;
+      .form-top {
+        .el-col {
+          width: 50%;
+          height: 50px;
+          text-align: center;
+          line-height: 50px;
+          background-color: #eeeeee;
+        }
+        .current {
+          border-top: 2px solid #ffa500;
+          background-color: #fff;
+        }
+      }
+    }
+    .form-content {
+      padding-top: 20px;
+      width: 290px;
+      margin: 0 auto;
+      .el-form {
+        position: relative;
+        width: 290px;
+        .el-form-item {
+          width: 290px;
+        }
+        .el-input {
+          width: 220px;
+        }
+        .el-button {
+          width: 220px;
+          margin-left: 68px;
+          font-size: 14px;
+          .el-icon-search {
+            margin-right: 5px;
+            font-size: 12px;
+          }
+        }
+        .change {
+          position: absolute;
+          top: 16px;
+          right: -21px;
+          height: 65px;
+          width: 22px;
+          border: 1px solid #cccccc;
+          border-top: 2px solid #cccccc;
+          border-bottom: 2px solid #cccccc;
+          border-left: none;
+          span {
+            position: absolute;
+            top: 21px;
+            right: -10px;
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+            color: #ffffff;
+            background-color: #999999;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+    .board {
+      margin-left: 16px;
+      width: 620px;
+      height: 350px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .service {
+    height: 58px;
+    padding: 8px 0px;
+    width: 100%;
+    background-color: #f5f5f5;
+    border: 1px solid #dddddd;
+    margin-bottom: 20px;
+    box-sizing: border-box;
+    .el-col {
+      width: 332px;
+      height: 42px;
+      line-height: 42px;
+      text-align: center;
+      &:not(:last-child) {
+        border-right: 1px solid #dddddd;
+      }
+      span {
+        display: inline-block;
+        vertical-align: top;
+      }
+      i {
+        font-size: 30px;
+      }
+      .iconbaozheng {
+        color: #00807f;
+      }
+      .iconweibiaoti-_huabanfuben,
+      .icondianhua {
+        color: #409eff;
+      }
+    }
+  }
+  .special {
+    width: 100%;
+    margin-bottom: 30px;
+    .special-top {
+      height: 30px;
+      line-height: 30px;
+      margin-bottom: 20px;
+      i {
+        float: left;
+        color: #409eff;
+        font-size: 20px;
+        margin-top: 2px;
+      }
+      h4 {
+        float: left;
+        margin-left: 5px;
+        font-size: 20px;
+        font-weight: normal;
+        color: #409eff;
+      }
+    }
+    .special-bottom {
+      height: 180px;
+      border: 1px solid #dddddd;
+      padding: 15px;
+      box-sizing: border-box;
+      .el-col {
+        position: relative;
+        width: 225px;
+        height: 150px;
+        margin: 0px 8px;
+        img {
+          width: 225px;
+          height: 150px;
+        }
+        p {
+          position: absolute;
+          bottom: 0;
+          z-index: 99;
+          width: 100%;
+          padding: 0px 10px;
+          height: 30px;
+          line-height: 30px;
+          background-color: rgba(0, 0, 0, 0.5);
+          color: #fff;
+          box-sizing: border-box;
+          .left {
+            float: left;
+          }
+          .right {
+            float: right;
+          }
+        }
+      }
+    }
+  }
+}
 </style>

2f22c97df8dea5ec47852c843bb0215bfd3d23f3
杨腾祺 <3150475761@qq.com>
2020-07-23 19:55:12 +0800

    35-机票列表组件创建和侧边栏静态完成

---
+++ components/air/flightsAside.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="asides">
+    <div class="asides-top">
+      <el-row>
+        <el-col :span="8">
+          <span class="iconfont iconweibiaoti-_huabanfuben"></span>
+          <p>航协认证</p>
+        </el-col>
+        <el-col :span="8">
+          <span class="iconfont iconbaozheng"></span>
+          <p>出行保证</p>
+        </el-col>
+        <el-col :span="8">
+          <span class="iconfont icondianhua"></span>
+          <p>7x24</p>
+        </el-col>
+      </el-row>
+      <div class="service">免费客服电话: 4006345678转2</div>
+    </div>
+    <div class="asides-bottom">
+      <p>历史查询</p>
+      <div class="history">
+        <div class="left">
+          <p>广州-北京</p>
+          <span>2020-07-24</span>
+        </div>
+        <div class="right">
+          <span class="btn">选择</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.asides-top {
+  width: 100%;
+  border: 1px solid #dddddd;
+  padding-top: 20px;
+  box-sizing: border-box;
+  .el-row {
+    text-align: center;
+  }
+  span {
+    font-size: 40px;
+  }
+  p {
+    font-size: 12px;
+  }
+  .iconweibiaoti-_huabanfuben {
+    color: #409eff;
+  }
+  .iconbaozheng {
+    color: #008000;
+  }
+  .icondianhua {
+    color: #409eff;
+  }
+  .service {
+    box-sizing: border-box;
+    height: 30px;
+    width: 100%;
+    background-color: #f6f6f6;
+    line-height: 30px;
+    padding-left: 8px;
+    margin-top: 20px;
+    font-size: 14px;
+  }
+}
+.asides-bottom {
+  width: 100%;
+  border: 1px solid #dddddd;
+  margin-top: 20px;
+  box-sizing: border-box;
+  padding: 0px 0px 10px 0px;
+  p {
+    padding: 10px 0px;
+    margin: 0px 8px;
+    border-bottom: 1px solid #dddddd;
+  }
+  .history {
+    height: 60px;
+    padding: 5px 0px 0px 0px;
+    margin: 0px 10px;
+    border: none;
+    .left {
+      float: left;
+      p {
+        border: none;
+        padding: 0px;
+        margin: 0px;
+      }
+      span {
+        font-size: 12px;
+        color: #888;
+      }
+    }
+    .right {
+      float: right;
+      p {
+        border: none;
+      }
+      .btn {
+        display: inline-block;
+        background-color: #ffa500;
+        width: 44px;
+        height: 20px;
+        margin-top: 13px;
+        font-size: 12px;
+        line-height: 20px;
+        text-align: center;
+        color: white;
+        border-radius: 5px;
+      }
+    }
+  }
+}
+</style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>机票列表</div>
+  <section class="container">
+    <el-row>
+      <el-col class="flights"></el-col>
+      <el-col class="aside">
+        <flightsAside />
+      </el-col>
+    </el-row>
+  </section>
 </template>
 
 <script>
-export default {};
+import flightsAside from "@/components/air/flightsAside";
+export default {
+  components: {
+    flightsAside
+  }
+};
 </script>
 
 <style lang="less" scoped>
+.container {
+  width: 1000px;
+  min-width: 1000px;
+  margin: 25px auto;
+  .el-col {
+    height: 1000px;
+  }
+  .flights {
+    width: 745px;
+  }
+  .aside {
+    margin-left: 17px;
+    width: 238px;
+  }
+}
 </style>
